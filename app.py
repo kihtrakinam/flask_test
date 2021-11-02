99aa08421fef83b20ba240e3fdb6044f64e5617a
Mani Karthik <maniennoreceg@gmail.com>
2021-11-02 20:09:04 +0530

    Read, Add and delete records from MySQL DB

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask import render_template, request, redirect
 ## Web Forms
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SelectField
 from wtforms.validators import DataRequired
 ## SQL
 from flask_sqlalchemy import SQLAlchemy
@@ -15,10 +15,10 @@ import urllib.parse as ups
 dbpass,dbuser,dbhost,dbname = ups.quote_plus(secret.dbpass),secret.dbuser,secret.dbhost,secret.dbname
 conn = "mysql+pymysql://{}:{}@{}/{}".format(dbuser,dbpass,dbhost,dbname)
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'SuperSecretKey'
-app.config['SQLALCHEMY_DATABASE_URI'] = conn
-db = SQLAlchemy(app)
+app_loc = Flask(__name__)
+app_loc.config['SECRET_KEY'] = 'SuperSecretKey'
+app_loc.config['SQLALCHEMY_DATABASE_URI'] = conn
+db = SQLAlchemy(app_loc)
 
 class participants(db.Model):
     id = db.Column(db.Integer,primary_key=True)
@@ -36,21 +36,21 @@ class ParticipantsForm(FlaskForm):
     first_name = StringField('First Name :',validators=[DataRequired()])
     last_name = StringField('Last Name :')
     age = StringField('Age :',validators=[DataRequired()])
-    gender = StringField('Gender :',validators=[DataRequired()])
+    gender = SelectField('Gender :',choices=[('M','Male'),('F','Female')],validators=[DataRequired()])
 
-@app.route('/')
+@app_loc.route('/')
 def index():
     return render_template('homepage.html',pageTitle='Flask server Home Page')
 
-@app.route('/Author')
+@app_loc.route('/Author')
 def author():
     return render_template('author.html',pageTitle='About Author')
 
-@app.route('/Calculator')
+@app_loc.route('/Calculator')
 def Calculator():
     return render_template('calc.html',display="",pageTitle='My Calculator')
 
-@app.route('/add',methods=['GET','POST'])
+@app_loc.route('/add',methods=['GET','POST'])
 def add():
     if request.method == 'POST':
         form = request.form
@@ -63,7 +63,7 @@ def add():
             return render_template('calc.html',display="Invalid entry",pageTitle='My Calculator')
     return redirect("/Calculator")
 
-@app.route('/add_participant',methods=['GET','POST'])
+@app_loc.route('/add_participant',methods=['GET','POST'])
 def add_participant():
     form = ParticipantsForm()
     if form.validate_on_submit():
@@ -75,10 +75,20 @@ def add_participant():
         return render_template('add_participant.html',form=form,Name=name,Age=age,Gender=gender,pageTitle='Add a participant')
     return render_template('add_participant.html',form=form,Name="",Age="",Gender="",pageTitle='Add a participant')
 
-@app.route('/all_participants')
+@app_loc.route('/all_participants')
 def all_participants():
     all_part = participants.query.all()
-    return render_template('all_participants.html',details=all_part,pageTitle='Flask server Home Page')
+    return render_template('all_participants.html',details=all_part,pageTitle='All Participants')
+
+@app_loc.route('/remove_participant/<int:participant_id>',methods=['GET','POST'])
+def remove_participant(participant_id):
+    if request.method == 'POST':
+        participant = participants.query.get_or_404(participant_id)
+        db.session.delete(participant)
+        db.session.commit()
+        return redirect('/all_participants')
+    else:
+        return redirect('/all_participants')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app_loc.run(debug=True)
